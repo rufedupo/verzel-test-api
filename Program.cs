bddadca847aa405cd48ceef1ac9b5e318c775020
Ruan Felipe Duarte Pontes <p.ruan.pontes@viveo.com.br>
2023-07-15 13:39:26 -0300

    Configurando o ambiente para aceitar o swagger.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,25 @@
+using verzel_test_api.config;
+
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+StartAPI(builder);
 
-app.Run();
+void StartAPI(WebApplicationBuilder builder)
+{
+    ConfigureServices(builder);
+    var app = builder.Build();
+    app.MapControllers();
+    app.UseSwagger();
+    app.UseSwaggerConfiguration();
+    app.UseCorsConfiguration();
+    app.Run();
+}
+
+void ConfigureServices(WebApplicationBuilder builder)
+{
+    builder.Services.AddDefaultConfiguration();
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddCorsConfiguration();
+    builder.Services.AddSwaggerConfiguration();
+}
---
+++ verzel-test-api.config/ApiConfiguration.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System.Text.Json.Serialization;
+
+namespace verzel_test_api.config
+{
+    public static class ApiConfiguration
+    {
+        public static void AddDefaultConfiguration(this IServiceCollection services)
+        {
+            services
+                .AddMemoryCache()
+                .AddControllers()
+                .AddJsonOptions(x =>
+                {
+                    x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+                    x.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
+                })
+                .ConfigureApiBehaviorOptions(options =>
+                {
+                    options.SuppressModelStateInvalidFilter = true;
+                });
+        }
+
+        public static void UseCorsConfiguration(this IApplicationBuilder app)
+        {
+            app.UseCors("AllowAll");
+        }
+
+        public static void AddCorsConfiguration(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                            builder =>
+                            {
+                                builder.AllowAnyOrigin()
+                                       .AllowAnyHeader()
+                                       .AllowAnyMethod();
+                            });
+            });
+        }
+
+        public static void UseSwaggerConfiguration(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "verzel-test-api v1");
+            });
+        }
+
+        public static void AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "verzel-test-api",
+                    Version = "v1",
+                    Description = "API of Managing Admin and Cars.",
+                });
+            });
+        }
+    }
+}
